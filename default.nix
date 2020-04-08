7bbf611f73e817cfd9e12d0881206b5a09f1eda8
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-04-08 11:18:34 +0200

    Add allHmModules.

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
-{
+rec {
   # home-manager modules
   hmModules = import ./hm-modules;
+  # A list of all modules in hmModules.
+  allHmModules = builtins.attrValues hmModules;
 
   id3 = pkgs.callPackage ./pkgs/id3 { };
   mopidy-podcast = pkgs.callPackage ./pkgs/mopidy-podcast { };
